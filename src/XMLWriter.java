95392ca528aa3c17295b98cfd130a4ea59cf9019
Gianna Nguyen <giannanguyenn@gmail.com>
2017-09-20 20:50:45 -0500

    XMLWriter.java added

---
+++ src/XMLWriter.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.List;
+import com.datacontainers.Person;
+import com.thoughtworks.xstream.XStream;
+
+public class XMLWriter {
+
+	public void xmlConverter(List<Person> persons) {
+		XStream  xstream = new XStream();
+		
+        File xmlOutput = new File("data/Persons.xml");
+		
+		PrintWriter xmlPrintWriter = null;
+		try {
+			xmlPrintWriter = new PrintWriter(xmlOutput);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		xmlPrintWriter.write("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n");
+		
+		xstream.alias("person", Person.class); 
+		for(Person aPerson : persons) {
+			// Use toXML method to convert Person object into a String
+			String personOutput = xstream.toXML(aPerson);
+			xmlPrintWriter.write(personOutput);
+		}
+		xmlPrintWriter.close();	
+	}
+}
