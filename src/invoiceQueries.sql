1599ae6df1677ad8d9c6fd8cda4567732f7782a2
vdoan98 <vdoan98@gmail.com>
2017-10-23 17:45:07 -0500

    Add invoiceQueries.sql

---
+++ src/invoiceQueries.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `Person`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Person`(
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
