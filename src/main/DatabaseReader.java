28519cb7bd9d2726db5edd0755a2f6671b08c9bd
vdoan98 <vdoan98@gmail.com>
2017-11-15 17:50:42 -0600

    Added database reader for Customer and Person

--- src/main/DatabaseReader.java
+++ src/main/DatabaseReader.java
@@ -1,6 +1,8 @@
 package main;
 
+import java.sql.*;
 import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -9,6 +11,7 @@ import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
+import com.ceg.ext.DatabaseInfo;
 import com.ceg.ext.InvoiceData;
 
 public class DatabaseReader {
@@ -22,29 +25,36 @@ public class DatabaseReader {
 
 	public LinkedList readPersons() {
 
+		Connection conn = DatabaseInfo.getConnection();
 
+		Person person;
+		Address address;
 
-		//TODO: replace with JDBC
-		while () {
-			
-			//TODO: Replace with construction of Address based on database
-			Address address = new Address();
+		String query = "SELECT p.person_id, p.person_code, p.last_name, p.first_name, a.street, a.city, a.state, a.zip, a.country FROM ((Person AS p JOIN PersonAddress AS pa ON p.person_id = pa.person_id) JOIN Address AS a ON pa.address_id = a.address_id)";
 
-			Person person;
+		try{
+			PreparedStatement ps = conn.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
 
-			// Check if there's email information. If not, create person object without
-			// email. 
-			if (data.length == 4) {
-				email = data[3];
-				//TODO: Construciton of Person based on database informaiton
-				person = new Person();
-			} else {
-				person = new Person();
+			while(rs.next()){
+				address = new Address(rs.getString("a.street"), rs.getString("a.city"), rs.getString("a.state"), rs.getString("a.zip"), rs.getString("a.country"));
+				person = new Person(rs.getString("p.person_code"), rs.getString("p.last_name"), rs.getString("p.first_name"), address);
+				String id = rs.getString("p.person_code");
+				String query2 = "SELECT email_address FROM (Person AS p JOIN Emails AS e ON p.person_id = e.person_id) WHERE p.person_code = ?";
+				ps = conn.prepareStatement(query2);
+				ps.setString(1, id);
+				rs = ps.executeQuery();
+				while(rs.next()){
+					person.addEmail(rs.getString("email_address"));
+				}
+				personList.addToEnd(person);
 			}
-			// Adds the Person object into Person ArrayList
-		
-			personList.addToEnd(person);
+			rs.close();
 
+		}catch (SQLException e){
+			System.out.println("SQLException: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
 
 		return personList;
@@ -53,39 +63,52 @@ public class DatabaseReader {
 	}
 
 	public LinkedList readCustomer() {
-		
-		//TODO: Replace with QUERY to get all information from database
-		while () {
 
-			int code = 0;
+		Connection conn = DatabaseInfo.getConnection();
 
-			for (int i = 0; i < personList.getSize(); i++) {
-				if (data[2].compareTo(((Person)personList.getObject(i)).getPersonCode()) == 0) {
-					code = i;
+		Customer customer;
+
+		//public Customer(String customerCode, char type, Person contact, String name, Address address) 
+		String query = "SELECT p.person_code, c.customer_code, c.customer_type FROM (Person AS p JOIN Customers AS c ON p.person_id = c.person_id)";
+
+		try{
+			PreparedStatement ps = conn.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()){
+				for (int i = 0; i < this.readPersons().getSize(); i++){
+					if(((Person)this.readPersons().getObject(i)).getPersonCode().compareToIgnoreCase(rs.getString("p.person_code")) == 0){
+						if (rs.getString("c.customer_type").charAt(0) == 'S'){
+							customer = new Student(rs.getString("c.customer_code"), rs.getString("c.customer_type").charAt(0),
+									((Person)this.readPersons().getObject(i)), ((Person)this.readPersons().getObject(i)).getFirstName() + ((Person)this.readPersons().getObject(i)).getLastName(),
+									((Person)this.readPersons().getObject(i)).getAddress());
+							customerList.addToEnd(customer);
+						}else if (rs.getString("c.customer_type").charAt(0) == 'G'){
+							customer = new General(rs.getString("c.customer_code"), rs.getString("c.customer_type").charAt(0),
+									((Person)this.readPersons().getObject(i)), ((Person)this.readPersons().getObject(i)).getFirstName() + ((Person)this.readPersons().getObject(i)).getLastName(),
+									((Person)this.readPersons().getObject(i)).getAddress());
+							customerList.addToEnd(customer);
+						}
+
+
+
+					}
 				}
 			}
+			rs.close();
 
-			if (data[1].charAt(0) == 'S') {
-				//TODO: Creation of new student based on information in database. 
-				Student customer = new Student();
-				// Adds the Person object into Person ArrayList
-
-				customerList.addToEnd(customer);
-			} else if (data[1].charAt(0) == 'G') {
-				//TODO: Construction of new customer based on database.
-				General customer = new General();
-				// Adds the Person object into Person ArrayList
-
-				customerList.addToEnd(customer);
-			}
-
+		}catch (SQLException e){
+			System.out.println("SQLException: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
 
-		sc.close();
+
 		return customerList;
-
-
 	}
+
+
+
+
 
 	public LinkedList readProducts() {
 
@@ -113,7 +136,7 @@ public class DatabaseReader {
 				Address address = new Address();
 				//TODO: Construction of new MovieTicket based on database. 
 				ticket = new MovieTicket();
-				
+
 				productList.addToEnd(ticket);
 				break;
 			case 'P':
@@ -125,7 +148,7 @@ public class DatabaseReader {
 			case 'R':
 				//TODO: Construction of new Refreshment based on database 
 				refresh = new Refreshment();
-				
+
 				productList.addToEnd(refresh);;
 				break;
 
@@ -147,22 +170,22 @@ public class DatabaseReader {
 		this.readPersons();
 		this.readCustomer();
 
-		
 
 
-			while () {
-			
-				DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
 
-				//TODO: Read in information for invoices. 
-				LinkedList totalProducts = new LinkedList();
+		while () {
 
-				
-				
-				invoiceList.addToEnd(invoice);
-			}
+			DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
 
-			return invoiceList;
+			//TODO: Read in information for invoices. 
+			LinkedList totalProducts = new LinkedList();
+
+
+
+			invoiceList.addToEnd(invoice);
+		}
+
+		return invoiceList;
 	}
 
 }
